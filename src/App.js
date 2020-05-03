614c2ff80f31bd0fb38807f11a17717c40327205
Sharath Deshabathni <sdeshaba@berkadia.com>
2020-05-03 17:37:58 +0530

    checking

--- src/App.js
+++ src/App.js
@@ -1,13 +1,13 @@
-import React from 'react';
-import logo from './logo.svg';
-import './App.css';
+import React from "react";
+import logo from "./logo.svg";
+import "./App.css";
 
 function App() {
   return (
     <div className='App'>
       <header className='App-header'>
         <img src={logo} className='App-logo' alt='logo' />
-        <p>Hello World! change for the PR</p>
+        <p>Hello World! change for check ddeployment</p>
         <a
           className='App-link'
           href='https://reactjs.org'
